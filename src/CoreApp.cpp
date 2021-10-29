201cff457d48fe97055b728a069b94d8133304c5
CreatorSiSo <creatorsisohd@gmail.com>
2021-10-29 15:49:28 +0200

    Setup initial glfw window.

--- src/CoreApp.cpp
+++ src/CoreApp.cpp
@@ -8,6 +8,16 @@ Texturia::CoreApp::CoreApp() {}
 Texturia::CoreApp::~CoreApp() {}
 
 void Texturia::CoreApp::Run() {
+  struct WindowProps {
+    std::string Title;
+    int Width;
+    int Height;
+
+    WindowProps(const std::string &title = "Texturia",
+                unsigned int width = 1600, unsigned int height = 900)
+        : Title(title), Width(width), Height(height) {}
+  };
+
   Texturia::Log::Init();
   TX_INFO("Initialized Logger");
 
@@ -16,8 +26,39 @@ void Texturia::CoreApp::Run() {
   WindowResizeEvent e(1280, 720);
   TX_TRACE(e);
 
-  while (true) {
+  WindowProps props;
+
+  TX_INFO("Creating Window {0} ({1} {2})", props.Title, props.Width,
+          props.Height);
+
+  GLFWwindow *window;
+
+  bool GLFWInitialized = false;
+  if (!GLFWInitialized) {
+    // TODO glfwTerminate() on system shutdown
+    int success = glfwInit();
+    TX_ASSERT(success, "Could not initialize glfw!");
+
+    GLFWInitialized = true;
   }
+
+  window = glfwCreateWindow(props.Width, props.Height, props.Title.c_str(),
+                            NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+  // glfwSetWindowUserPointer(window, NULL);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
+
+  glfwTerminate();
 }
 
 Texturia::CoreApp *Texturia::CreateCoreApp() { return new CoreApp; }
