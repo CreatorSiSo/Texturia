55477ad7d7a042c40776e9951326deefcfdcdeda
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-11-07 00:37:05 +0100

    Disable imguis custom opengl loader.

--- src/ImGui/ImGuiBuild.cpp
+++ src/ImGui/ImGuiBuild.cpp
@@ -1,5 +1,7 @@
 #include "txpch.hpp"
 
-#define IMGUI_IMPL_OPENGL_LOADER_GLAD
+// We use glad as a opengl loader so we dont need imguis custom one
+#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+
 #include "backends/imgui_impl_glfw.cpp"
 #include "backends/imgui_impl_opengl3.cpp"
